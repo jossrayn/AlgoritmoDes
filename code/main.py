760f4a964d3868e12fa76e268148d6b5801d9e27
jossrayn <rayn0419@gmail.com>
2020-08-17 21:35:27 -0600

    reestructuración

--- code/main.py
+++ code/main.py
@@ -6,6 +6,12 @@ from tkinter import Text
 from PIL import Image, ImageTk
 import os.path
 
+import ClaveDes
+
+from Des2 import *
+
+from Des3 import *
+
 def mainWindow():
     global main_window
     main_window=Tk()
@@ -34,29 +40,22 @@ def mainWindow():
     label = Label(main_window,text="Proceso del algoritmo", fg="#000000",bg="#c1e8d5",font=('Corbel',20)).place(x=150,y=163)
     log = Listbox (main_window,fg="#000000",bg="#ffffff",height=29,width=95)
 
-    scrollbar = Scrollbar(main_window)
-    scrollbar.pack(side = RIGHT, fill = BOTH)
-
     # Insert elements into the listbox 
     for values in range(100): 
         log.insert(END, values) 
 
     log.place(x=159,y=217)
-    # Attaching Listbox to Scrollbar 
-    # Since we need to have a vertical  
-    # scroll we use yscrollcommand 
-    log.config(yscrollcommand = scrollbar.set) 
-      
-    # setting scrollbar command parameter  
-    # to listbox.yview method its yview because 
-    # we need to have a vertical view 
-    scrollbar.config(command = log.yview) 
-
 
     #Mensaje encriptado
     label = Label(main_window,text="Mensaje encriptado", fg="#000000",bg="#c1e8d5",font=('Corbel',18)).place(x=350,y=710)
     encrypted=StringVar()
     entry_message_encrypted=Text(main_window,height=2,width=52,bg="#ffffff",state=DISABLED,fg="#0ECA9B",font=('Corbel',16)).place(x=157,y=760)
+
+    #botón de play
+    img_btn_play=Image.open("../resources/play.png")
+    img_resize_btn_play = img_btn_play.resize((30, 30), Image.ANTIALIAS)
+    img_btn_play_aux = ImageTk.PhotoImage(img_resize_btn_play)
+    btn_play=Button(main_window,bg="white",image=img_btn_play_aux,relief=FLAT,highlightthickness=0, bd=0).place(x=430,y=115)
 
 
     main_window.mainloop()
