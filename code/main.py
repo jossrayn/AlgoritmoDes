75b3bea23f8df72745058f0eeb562f924f2afdce
jossrayn <rayn0419@gmail.com>
2020-08-17 23:21:02 -0600

    cifrado listo

--- code/main.py
+++ code/main.py
@@ -6,11 +6,7 @@ from tkinter import Text
 from PIL import Image, ImageTk
 import os.path
 
-import ClaveDes
-
-from Des2 import *
-
-from Des3 import *
+from encriptar import *
 
 def mainWindow():
     global main_window
@@ -38,26 +34,42 @@ def mainWindow():
     entry_key=Entry(main_window,textvariable=key,width=40,relief=FLAT, bg="#ffffff",fg="#0ECA9B",font=('Corbel',12)).place(x=512,y=120)
 
     label = Label(main_window,text="Proceso del algoritmo", fg="#000000",bg="#c1e8d5",font=('Corbel',20)).place(x=150,y=163)
+    global log
     log = Listbox (main_window,fg="#000000",bg="#ffffff",height=29,width=95)
-
-    # Insert elements into the listbox 
-    for values in range(100): 
-        log.insert(END, values) 
 
     log.place(x=159,y=217)
 
     #Mensaje encriptado
     label = Label(main_window,text="Mensaje encriptado", fg="#000000",bg="#c1e8d5",font=('Corbel',18)).place(x=350,y=710)
     encrypted=StringVar()
-    entry_message_encrypted=Text(main_window,height=2,width=52,bg="#ffffff",state=DISABLED,fg="#0ECA9B",font=('Corbel',16)).place(x=157,y=760)
+    global entry_message_encrypted
+    entry_message_encrypted=Text(main_window,height=2,width=52,bg="#ffffff",state=DISABLED,fg="#0ECA9B",font=('Corbel',16))
+    
+    entry_message_encrypted.place(x=157,y=760)
 
     #botón de play
     img_btn_play=Image.open("../resources/play.png")
     img_resize_btn_play = img_btn_play.resize((30, 30), Image.ANTIALIAS)
     img_btn_play_aux = ImageTk.PhotoImage(img_resize_btn_play)
-    btn_play=Button(main_window,bg="white",image=img_btn_play_aux,relief=FLAT,highlightthickness=0, bd=0).place(x=430,y=115)
+    btn_play=Button(main_window,bg="white",image=img_btn_play_aux,relief=FLAT,highlightthickness=0, bd=0,command=execute).place(x=430,y=115)
 
 
     main_window.mainloop()
+
+def execute():
+    if(message.get()!="" and key.get()!=""):
+        cypher = Encriptar(key.get(),message.get())
+        cypher.main()
+        values = cypher.logFile
+        # Insert elements into the listbox 
+        for value in values: 
+            log.insert(END, value) 
+
+        cypher_message = values[-1]
+
+        entry_message_encrypted.insert(tk.END,cypher_message)
+
+    else:
+        messagebox.showerror(title="Error! Espacios incompletos.", message="¨Por favor complete los campos de mensaje y clave.")
 
 mainWindow()
